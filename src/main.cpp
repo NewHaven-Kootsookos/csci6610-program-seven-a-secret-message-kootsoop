a6fd263b502055a5439882759fe67217cc62bd88
kootsoop <peter@kootsoop.com>
2022-04-10 13:55:14 -0400

    Attempt to decode

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
+
+void encode(ifstream& plain)
+{
+        unsigned char a, b, c, d;
+        for (;;)
+        {
+                int n = plain.get(); // ABCDEFGH
+                if (plain.eof())
+                {
+                        break;
+                }
+                a = n >> 3; // 000ABCDE
+                b = (n & 0x7); // 00000FGH
+                c = (a ^ b) << 3; // AB(C^F)(D^G)(E^H)000
+                d = c | b; // AB(C^F)(D^G)(E^H)FGH
+                cout << (unsigned char) d;
+        }
+}
 
 int main(void)
 {
-    std::cout << "Hello world!" << std::endl;
+        ifstream input;
 
-    return 0;
+        input.open("messCypher.txt");
+
+        encode(input);
+
 }
